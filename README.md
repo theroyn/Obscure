b818b1c09d016c67b911c7c781b1e39d0d01a73d
Roy Nevo <theroyn@gmail.com>
2020-09-24 04:08:40 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Obscure C++ code
+
+A showcase of some horrible yet perfectly valid C++ code.
---
+++ source.cxx
@@ -0,0 +1,27 @@
+%:include <iostream>
+
+%:define φρλ 5
+%:define Σ printf
+%:define double int
+%:define program main
+%:define GaRbAgE(token) char token[]
+
+double program()
+<%
+    double write = 2;
+    GaRbAgE(string) = "he*lo world";
+    volatile int x = φρλ;
+    if (not true);
+    <%
+        Σ("what is the truth? %d\n", (double)write[string]);
+    %>
+    x = 6;
+    if (x > 5)
+    <%
+        float λ = 3.f;
+        int ρ = 6;
+        int φ = ρ bitor (1 << 3);
+        printf("hihi %f %d %d\n", λ, ρ, φ);
+    %>
+%>
+
